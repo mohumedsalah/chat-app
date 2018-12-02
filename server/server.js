eb479a30b1405b69b612194f8950f1513c3a4c63
Mohumed Salah <mohumed.s.omar@gmail.com>
2018-12-02 13:45:29 +0200

    socke communication

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express')
+const socketIO = require('socket.io')
 const port = 3000
 
 const publicpath = path.join(__dirname ,"/../public")
 var app = express()
+var server = http.createServer(app)
+var io = socketIO(server);
 
 
 app.use(express.static(publicpath))
 
+io.on('connection',(socket)=>{
+    console.log('New User connected');
+    socket.on('disconnect',()=>{
+        console.log('client is disconnected');
+    })
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 console.log(publicpath);
